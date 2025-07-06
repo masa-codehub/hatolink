2b5c11021c354aee7d9ad497c6a7371cddc4f455
masatake <mastake.home@outlook.jp>
2025-07-06 19:47:50 +0900

    feat: 定期実行トリガーの設定をコードで管理し、冪等性を保証するルールを追加

---
+++ hatolink/infrastructure/triggers.js
@@ -0,0 +1,39 @@
+// triggers.js
+// US-001-Infra-Trigger: PostScheduledTweetsUseCaseの定期実行トリガー設定
+
+const { PostScheduledTweetsUseCase } = require('../usecase/PostScheduledTweetsUseCase');
+const { SpreadsheetTweetRepository } = require('../adapter/db/SpreadsheetTweetRepository');
+const { TwitterApiAdapter } = require('../adapter/api/TwitterApiAdapter');
+
+/**
+ * PostScheduledTweetsUseCaseを実行するエントリーポイント
+ */
+function runPostScheduledTweets() {
+  const tweetRepository = new SpreadsheetTweetRepository();
+  const twitterApi = new TwitterApiAdapter();
+  const useCase = new PostScheduledTweetsUseCase(tweetRepository, twitterApi);
+  useCase.execute();
+}
+
+/**
+ * 10分ごとのトリガーを冪等に設定する
+ * 既存の同名トリガーを削除してから新規作成
+ */
+function createTrigger() {
+  const functionName = 'runPostScheduledTweets';
+  const triggers = ScriptApp.getProjectTriggers();
+  triggers.forEach(trigger => {
+    if (trigger.getHandlerFunction() === functionName) {
+      ScriptApp.deleteTrigger(trigger);
+    }
+  });
+  ScriptApp.newTrigger(functionName)
+    .timeBased()
+    .everyMinutes(10)
+    .create();
+}
+
+module.exports = {
+  runPostScheduledTweets,
+  createTrigger,
+};
